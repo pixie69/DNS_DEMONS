4905fe4dfc68da52c48400c11c6491fc81faa7e6
Nachiket <nachiketdodia33@gmail.com>
2023-04-21 00:49:07 +0530

    added another protocol

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function sendPackets() {
+function sendPackets() {
   let numOfPackets = 0;
   let corrupted = [];
   const screen = document.getElementById("animation-screen");
@@ -11,25 +11,46 @@ async function sendPackets() {
   }
   console.log(numOfPackets);
   console.log(corrupted);
-  for (let i = 1; i <= numOfPackets; i++) {
-    const isCorrupt = corrupted.indexOf(data[i-1]) !== -1;
-    const packet = (
-      `<div class="row-item">
-      ${isCorrupt?`<div class=\"packet corrupt\">${data[i-1]}</div>`:`<div class=\"packet\">${data[i-1]}</div>`}
-      </div>`
-    );
-    screen.innerHTML += packet;
+  for (let i = 0; i < numOfPackets; i++) {
+    const isCorrupt = corrupted.indexOf(data[i]) !== -1;
+      const packet = (
+        `<div class="row-item">
+        ${isCorrupt?`<div class=\"packet corrupt\">${data[i]}</div>`:`<div class=\"packet\">${data[i]}</div>`}
+        </div>`
+      );
+      screen.innerHTML += packet;
   }
+
   const packets = document.getElementsByClassName("packet");
-  for (let i = 0; i < packets.length; i++) {
-    const element = packets[i];
-    const isCorrupt = element.classList.contains("corrupt");
-    element.style.border = isCorrupt ? "1px solid red" : "1px solid black";
-    element.style.animation = isCorrupt
-      ? "corruptedAnimation"
-      : "normalAnimation";
-    element.style.animationDuration = "4s";
-    element.style.animationDelay = `${i * 4000}ms`;
+
+  // For Stop-N-Wait uncomment the following lines =>
+  // 
+  // for (let i = 0; i < packets.length; i++) {
+  //   const element = packets[i];
+  //   const isCorrupt = element.classList.contains("corrupt");
+  //   element.style.border = isCorrupt ? "1px solid red" : "1px solid black";
+  //   element.style.animation = isCorrupt
+  //     ? "corruptedAnimation"
+  //     : "normalAnimation";
+  //   element.style.animationDuration = "4s";
+  //   element.style.animationDelay = `${i * 4000}ms`;
+  // }
+
+  // For Go-Back-N-Wait uncomment the following lines =>
+
+  for (let i = 0; i < Math.ceil(packets.length / 3); i++) {
+    for (let j = 3 * i; j < Math.max(packets.length, 3 * i + 3); j++) {
+      const element = packets[j];
+      const isCorrupt = element.classList.contains("corrupt");
+      element.style.border = isCorrupt ? "1px solid red" : "1px solid black";
+      element.style.animation = isCorrupt
+        ? "corruptedAnimation"
+        : "normalAnimation";
+      element.style.animationDuration = "4s";
+      element.style.animationDelay = `${i * 4000}ms`;
+    }
   }
-  screen.innerHTML+=`<div class="row-item" style="text-align:center;">Total number of transmissions: ${corrupted.length+numOfPackets}</div>`;
+  screen.innerHTML += `<div class="row-item" style="text-align:center;">Total number of transmissions: ${
+    corrupted.length + numOfPackets
+  }</div>`;
 }
